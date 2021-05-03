523cd42fdb3959332115af7adef14c5a1f151a23
edwinalin <ta517152139@gmail.com>
2021-05-03 15:53:55 +0800

    ＃下拉選單教學

---
+++ app/src/main/java/com/example/spinnerexample/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.spinnerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.spinnerexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val balls = listOf("Baseball", "Basketball", "Football", "Other")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //////spinner//////
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, balls)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item) //設定下拉式選單
+        binding.spinner.adapter = adapter
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selected = parent?.getItemAtPosition(position).toString()
+                binding.TVResult.text = getString(R.string.Selection, selected)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+    }
+
+}
